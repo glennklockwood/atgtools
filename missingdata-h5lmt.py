821bc8c1faff57cbf34003d9df8f5155dfee53f1
Glenn K. Lockwood <glennklockwood@gmail.com>
2016-03-29 09:05:32 -0700

    print the data before/after a missing data point in h5lmt files

--- missingdata-h5lmt.py
+++ missingdata-h5lmt.py
@@ -11,10 +11,29 @@ filepath = os.path.abspath( sys.argv[1] )
 f = h5py.File( filepath, 'r' )
 
 read_bytes = 0
-ost = 0
-for ost_row in f['FSMissingGroup/FSMissingDataSet']:
-    for timestep, value in enumerate( ost_row ):
-        if value != 0:
-            print ost, timestep, f['FSStepsGroup/FSStepsDataSet'][timestep], value
+ost_num = 0
+h5_dataset = f['FSMissingGroup/FSMissingDataSet']
+for ost_row in h5_dataset:
+    for timestep_num, value in enumerate( ost_row ):
+        if value == 0:
+            continue
 
-    ost += 1
+        val_after = -1.0
+        val_before= -1.0
+        if timestep_num > 0:
+            val_before= f['OSTReadGroup/OSTBulkReadDataSet'][ost_num,timestep_num-1]
+        if timestep_num+1 < h5_dataset.shape[1]:
+            val_after = f['OSTReadGroup/OSTBulkReadDataSet'][ost_num,timestep_num+1]
+
+        # (57, 17270, 1455177550, 1, 1316454.3999999999, 55705.599999999999, 1344307.2)
+        print "%3d %5d %10d %2d %12.2f %12.2f %12.2f" % ( 
+            ost_num, 
+            timestep_num,
+            f['FSStepsGroup/FSStepsDataSet'][timestep_num],
+            value,
+            val_before,
+            f['OSTReadGroup/OSTBulkReadDataSet'][ost_num,timestep_num],
+            val_after
+        )
+
+    ost_num += 1
