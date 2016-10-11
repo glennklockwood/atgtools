702cdf42df53d5c0204dd9b67adf2a4e667edf4c
Glenn K. Lockwood <glock@lbl.gov>
2016-10-11 13:02:05 -0700

    added support for anonymizing ISDCT logs and joining of a device:node map

--- parse_dct_stats.py
+++ parse_dct_stats.py
@@ -14,8 +14,48 @@ import sys
 import pandas
 import numpy as np
 import warnings
+import hashlib
 
-def parse_dct_counters_file( path ):
+ANONYMIZE_SN = True
+
+def anonymize_sn( sn ):
+    """
+    Anonymize the serial number of each device, but preserve its enumerated id
+    (0 or 1 for Intel DC P3608)
+    """
+    if ANONYMIZE_SN:
+        sn, devid = sn.split('-',1)
+        hash = hashlib.md5()
+        hash.update(sn)
+        return (hash.hexdigest() + "-" + devid).strip()
+    else:
+        return sn
+
+def load_dev_map( path ):
+    """
+    Load map containing serial numbers and node names.  Each line in this device
+    map file should have a format
+
+        CVF000000000000000-1 nid00000
+
+    with the device serial number, a space, and the node to which it belongs
+    """
+    dev_map = {}
+    if path is None:
+        warnings.warn("Unable to load device map file")
+        return dev_map
+    try:
+        with open( path, 'r' ) as fp:
+            for line in fp:
+                sn, node = line.split(None, 1)
+                sn = anonymize_sn( sn )
+                dev_map[sn] = node.strip()
+    except IOError:
+        warnings.warn("Unable to load device map file")
+        pass
+    return dev_map
+
+def parse_dct_counters_file( path, dev_map=None ):
     """Read the output of a single isdct command.  Understands the output of
     the following options:
       isdct show -smart (SMART attributes)
@@ -33,7 +73,11 @@ def parse_dct_counters_file( path ):
             if device_sn is None:
                 rex_match = re.search( '(Intel SSD|SMART Attributes).*(CVF[^ ]+-\d+)', line )
                 if rex_match is not None:
-                    device_sn = rex_match.group(2)
+                    device_sn = anonymize_sn(rex_match.group(2))
+                    if device_sn in dev_map:
+                        data['NodeName'] = dev_map[device_sn]
+                    else:
+                        data['NodeName'] = "unknown"
                     if rex_match.group(1) == "Intel SSD":
                         parse_mode = 0
                     elif rex_match.group(1) == "SMART Attributes":
@@ -53,7 +97,7 @@ def parse_dct_counters_file( path ):
                     data[key] = val.strip()
 
     if device_sn is None:
-        warnings.warn("Couldn't find device sn")
+        warnings.warn("Couldn't find device sn in " + path)
     else:
         return { device_sn : data }
 
@@ -78,17 +122,24 @@ def find_duplicate_keys( data_list ):
 
     return duplicate_kv
 
-def parse_many_dct_counters_files( file_list ):
-    """Receives a list of file paths and parses all input files.  The counters
+def parse_many_dct_counters_files( file_list, dev_map_file=None ):
+    """
+    Receives a list of file paths and parses all input files.  The counters
     from each file are aggregated based on the NVMe device serial number, with
     redundant counters being overwritten.
     
-    Returns a dict of dicts containing all counters keyed by serial number."""
+    Returns a dict of dicts containing all counters keyed by serial number.
+    """
+
+    dev_map = load_dev_map(dev_map_file)
 
     all_data = {}
     for f in file_list:
-        parsed_counters = parse_dct_counters_file(f)
-        if len(parsed_counters.keys()) > 1:
+        parsed_counters = parse_dct_counters_file(f, dev_map)
+        if parsed_counters is None:
+            warnings.warn("No valid counters found in " + f)
+            continue
+        elif len(parsed_counters.keys()) > 1:
             raise Exception("Received multiple serial numbers from parse_dct_counters_file")
         else:
             device_sn = parsed_counters.keys()[0] 
@@ -124,11 +175,13 @@ def parse_many_dct_counters_files( file_list ):
     return all_data
 
 def counters_dict_to_numeric_dataframe( input_dict ):
-    """Transforms the data from either parse_dct_counters_file or
-    parse_many_dct_counters_files into a dataframe with only the numeric
-    columns extracted.  All non-numeric columns are dropped."""
-    df = pandas.DataFrame.from_dict(input_dict, orient='index') \
-                         .apply(pandas.to_numeric, errors='coerce')
+    """
+    Transforms the data from either parse_dct_counters_file or
+    parse_many_dct_counters_files into a dataframe with only the numeric columns
+    extracted.  All non-numeric columns are dropped except for node name
+    """
+    df = pandas.DataFrame.from_dict(input_dict, orient='index')
+    numeric_df = df.apply(pandas.to_numeric, errors='coerce')
     numeric_keys = []
     for i in df:
         if len(df[i].nonzero()[0]) == 0:
@@ -139,9 +192,13 @@ def counters_dict_to_numeric_dataframe( input_dict ):
             continue
         numeric_keys.append( i )
 
+    if 'NodeName' in df:
+        df['NodeName'] = df['NodeName']
+        numeric_keys = [ 'NodeName' ] + numeric_keys
     return df[numeric_keys]
 
 if __name__ == '__main__':
-    all_data = parse_many_dct_counters_files( sys.argv[1:] )
+    all_data = parse_many_dct_counters_files( sys.argv[1:], 'dev_map.txt' )
     df = counters_dict_to_numeric_dataframe( all_data )
+    df.index.name = "DeviceID"
     print df.to_csv()
