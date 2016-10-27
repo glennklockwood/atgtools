7de843a815706a6e74be189685e55bd9b17a0fbf
Glenn K. Lockwood <glock@lbl.gov>
2016-10-27 14:06:37 -0700

    fixed some bugs
    1. some counters were being interpreted as hex when they should've been decimal
    2. smart counters were not being named properly

--- parse_dct_stats.py
+++ parse_dct_stats.py
@@ -55,6 +55,21 @@ def load_dev_map( path ):
         pass
     return dev_map
 
+def rekey_smart_buffer(smart_buffer):
+    """
+    Take a buffer containing smart values associated with one register and
+    create unique counters
+    """
+    data = {}
+    prefix = smart_buffer.get("Description")
+    if prefix is None:
+        prefix = smart_buffer.get("_id")
+
+    for key, val in smart_buffer.iteritems():
+        key = ("%s_%s" % (prefix, key.strip())).replace("-","").replace(" ", "_")
+        data[key] = val.strip()
+    return data
+
 def parse_dct_counters_file( path, dev_map=None ):
     """Read the output of a single isdct command.  Understands the output of
     the following options:
@@ -64,12 +79,14 @@ def parse_dct_counters_file( path, dev_map=None ):
       isdct show -a (drive info)
     Outputs a dict of dicts keyed by the device serial number.
     """
+
     data = {}
     device_sn = None
     parse_mode = 0      # =0 for regular counters, >1 for SMART data
-    counter_prefix = "" # for SMART data
+    smart_buffer = {}
     with open( path, 'r' ) as fp:
         for line in fp:
+            line = line.strip()
             if device_sn is None:
                 rex_match = re.search( '(Intel SSD|SMART Attributes).*(CVF[^ ]+-\d+)', line )
                 if rex_match is not None:
@@ -88,13 +105,15 @@ def parse_dct_counters_file( path, dev_map=None ):
                 key, val = line.split(':')
                 data[key.strip()] = val.strip()
             elif parse_mode > 0 and ':' in line:
-                if line.startswith('Description'):
-                    counter_prefix = line.split(None, 2)[2].strip()
-                    parse_mode += 1
-                else:
-                    key, val = line.split(':')
-                    key = ( "%s_%s" % ( counter_prefix, key.strip() ) ).replace(" ", "_")
-                    data[key] = val.strip()
+                key, val = line.split(':')
+                smart_buffer[key.strip()] = val.strip()
+            elif parse_mode > 0 and line.startswith('-') and line.endswith('-'):
+                for key, val in rekey_smart_buffer(smart_buffer).iteritems():
+                    data[key.strip()] = val
+                smart_buffer = { '_id' : line.split()[1] }
+        if parse_mode > 0: # flush the last SMART register
+            for key, val in rekey_smart_buffer(smart_buffer).iteritems():
+                data[key] = val
 
     if device_sn is None:
         warnings.warn("Couldn't find device sn in " + path)
@@ -143,6 +162,7 @@ def parse_many_dct_counters_files( file_list, dev_map_file=None ):
             raise Exception("Received multiple serial numbers from parse_dct_counters_file")
         else:
             device_sn = parsed_counters.keys()[0] 
+
         ### merge file's counter dict with any previous counters we've parsed
         if device_sn not in all_data:
             all_data[device_sn] = parsed_counters[device_sn]
@@ -153,18 +173,19 @@ def parse_many_dct_counters_files( file_list, dev_map_file=None ):
     for device_sn, counters in all_data.iteritems():
         for counter, value in counters.iteritems():
             new_value = None
-            try:
-                new_value = long(value)
-            except ValueError:
-                pass
-            try:
-                new_value = float(value)
-            except ValueError:
-                pass
-            try:
-                new_value = long(value, 16)
-            except ValueError:
-                pass
+            ### first, handle counters that do not have an obvious way to cast
+            if counter in ("Temperature", "Thermal_Throttle_Status_ThrottleStatus"):
+                value = value.split()[0]
+
+            ### the order here is important, but hex that is not prefixed with
+            ### 0x may be misinterpreted as integers.  if such counters ever
+            ### surface, they must be explicitly cast above
+            for cast in ( long, float, lambda x: long(x,16) ):
+                try:
+                    new_value = cast(value)
+                    break
+                except ValueError:
+                    pass
             if value == "True":
                 new_value = True
             elif value == "False":
@@ -183,17 +204,20 @@ def counters_dict_to_numeric_dataframe( input_dict ):
     df = pandas.DataFrame.from_dict(input_dict, orient='index')
     numeric_df = df.apply(pandas.to_numeric, errors='coerce')
     numeric_keys = []
-    for i in df:
-        if len(df[i].nonzero()[0]) == 0:
-            continue
+    for i in sorted(df.keys()):
+        ### don't print counters that are non-numeric
         if df[i].dtype != np.int64 and df[i].dtype != np.float64:
             continue
-        if len(df[i].unique()) == 1:
+
+        ### don't print counters that are the same for all devices. df.count > 1
+        ### because we can't tell how many rows are unique if there's only one
+        ### row
+        if df.shape[0] > 1 and len(df[i].unique()) == 1:
             continue
+
         numeric_keys.append( i )
 
     if 'NodeName' in df:
-        df['NodeName'] = df['NodeName']
         numeric_keys = [ 'NodeName' ] + numeric_keys
     return df[numeric_keys]
 
